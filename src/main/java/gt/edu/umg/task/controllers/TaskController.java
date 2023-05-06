11d66a1e51a7d5300804d8b984c4e99e8d5882ce
WilliamRCR <williamcastanazaromero2@gmail.com>
2023-05-06 10:18:36 -0600

    Creacion de aplicacion

---
+++ src/main/java/gt/edu/umg/task/controllers/TaskController.java
@@ -0,0 +1,42 @@
+package gt.edu.umg.task.controllers;
+
+import gt.edu.umg.task.entities.Task;
+import gt.edu.umg.task.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    @Autowired
+    TaskService taskService;
+
+    @GetMapping
+    public List<Task> findAll(){
+        return taskService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Task findById(@PathVariable Long id){
+        return taskService.findById(id);
+    }
+
+    @PostMapping()
+    public Task createTask(@RequestBody Task task){ taskService.create(task);
+        return task;
+    }
+
+    @PutMapping(value = "/{id}")
+    public Task modifyTask(@RequestBody Long id,  Task task){
+        taskService.modify(id, task);
+        return task;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteProduct(@PathVariable Long id){ taskService.delete(id);
+
+    }
+}
---
+++ src/main/java/gt/edu/umg/task/services/TaskServiceImpl.java
@@ -0,0 +1,48 @@
+package gt.edu.umg.task.services;
+
+import gt.edu.umg.task.daos.TaskDao;
+import gt.edu.umg.task.entities.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskServiceImpl implements TaskService{
+
+    @Autowired
+    TaskDao taskDao;
+
+    @Override
+    public List<Task> findAll() {
+        return (List<Task>)taskDao.findAll();
+    }
+
+    @Override
+    public Task findById(Long id) {
+        return taskDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public Task create(Task task) {
+        taskDao.save(task);
+        return task;
+    }
+
+    @Override
+    public Task modify(Long id, Task task) {
+        if (taskDao.existsById(id)) {
+            task.setId(id);
+            taskDao.save(task);
+        }
+        return task;
+    }
+
+    @Override
+    public void delete(Long id) {
+        if(taskDao.existsById(id)){
+            taskDao.deleteById(id);
+        }
+
+    }
+}
